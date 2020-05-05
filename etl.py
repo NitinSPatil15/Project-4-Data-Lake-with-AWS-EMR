024a3b8c3d97a8f319886fcf72d9e213ad781411
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 17:51:34 +0530

    Importing libraries and configurations

---
+++ etl.py
@@ -0,0 +1,17 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format, dayofweek
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+from pyspark.sql.functions import row_number, monotonically_increasing_id
+from pyspark.sql import Window
+
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
